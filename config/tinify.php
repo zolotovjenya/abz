e48f44d6c62486fd93b00b136d7c968f91bf78c0
root <root@axisway.com>
2022-07-01 10:49:48 +0200

    added config

---
+++ config/tinify.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tinify Service Key
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for tinify service.
+    |
+    */
+
+    'apiKey' => 'TVQ4NgYfs86FJFRVxLZslbwjP6zFhtt6',
+    'firstlyIcon' => 'https://i.pinimg.com/474x/76/4d/59/764d59d32f61f0f91dec8c442ab052c5.jpg'
+
+];
