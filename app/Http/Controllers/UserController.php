bc7ab9dfff04dd10537920ba299c795fbb7a9f7c
root <root@axisway.com>
2022-07-01 15:51:29 +0200

    edited response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
 
 class UserController extends Controller
 {
@@ -18,5 +20,23 @@ class UserController extends Controller
         $users = $user->with('position')->paginate($total);
 
     	return response()->json($users);
+    }
+
+    public function user(Request $request){
+        $validator = Validator::make($request->route()->parameters(), [
+            'id' => 'required|numeric'
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = new User();
+        $data = $user->where('id', $request->route('id'))->first();
+        if(!$data){
+            abort(500);
+        }
+
+    	return response()->json(['success' => true, 'user' => $data]);
     }
 }
